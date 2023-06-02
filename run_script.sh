79842bb59c5ce999326053ba4f737d85ab64316b
damodaran <g.damodaran26@gmail.com>
2023-06-02 20:57:52 +1000

    update run script

--- run_script.sh
+++ run_script.sh
@@ -4,4 +4,6 @@ docker -v
 echo "Building docker image"
 docker build --tag karate:1.0 .
 docker run --rm --volume $PWD:/usr/src/app --user "$UID:$GROUPS" karate:1.0
+echo $PWD
+ls
 docker rmi karate:1.0
